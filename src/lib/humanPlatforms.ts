e6566ab502ef6f4674d7f93fd2978da3be7dcc82
Kirill Markin <markinkirill@gmail.com>
2026-04-12 08:56:20 +0200

    Remove country code from App Store URL

--- src/lib/humanPlatforms.ts
+++ src/lib/humanPlatforms.ts
@@ -25,7 +25,7 @@ export type DisabledHumanPlatform = {
 export type HumanPlatform = ActiveHumanPlatform | DisabledHumanPlatform;
 
 export const APP_STORE_URL =
-  "https://apps.apple.com/us/app/flashcards-open-source-app/id6760538964";
+  "https://apps.apple.com/app/flashcards-open-source-app/id6760538964";
 
 export const GOOGLE_PLAY_URL =
   "https://play.google.com/store/apps/details?id=com.flashcardsopensourceapp.app&pcampaignid=web_share";
