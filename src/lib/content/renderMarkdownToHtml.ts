ca560c56b8bab7818930e9db8f0edb26d0dd3a52
Kirill Markin <markinkirill@gmail.com>
2026-04-19 08:48:33 +0200

    Enable GFM markdown rendering

--- src/lib/content/renderMarkdownToHtml.ts
+++ src/lib/content/renderMarkdownToHtml.ts
@@ -1,7 +1,8 @@
 import { remark } from "remark";
+import gfm from "remark-gfm";
 import html from "remark-html";
 
 export async function renderMarkdownToHtml(markdown: string): Promise<string> {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark().use(gfm).use(html).process(markdown);
   return result.toString();
 }
